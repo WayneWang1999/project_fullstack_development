4f72d1173007c6429a56a9dbc4a508ec7393f6f5
Henrique <machittes@yahoo.com.br>
2024-10-06 15:13:07 -0400

    changes login asnd order page (driver)

--- routes/driver_frontend.js
+++ routes/driver_frontend.js
@@ -15,6 +15,32 @@ router.get('/', async (req, res) => {
     res.render('drivers/login');
 });
 
+router.post('/login', async (req, res) => {
+
+    const { email, password } = req.body;
+
+    try {
+
+        // Check if a user with the provided email exists
+        const driver = await Driver.findOne({ email: { $regex: new RegExp('^' + email + '$', 'i') } });
+        if (!driver) {
+            // Return error if no user with the given email
+            return res.status(400).render('drivers/login', { error: 'Invalid email' });
+        }
+    ;
+
+        //  fetch the order data
+        const orders = await Order.find().populate('customer').populate('driver').populate('order_Menus.menu');
+
+
+        // Render the owner's dashboard to a layout with fetched data
+        return res.render('owners/layout', { orders });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server error');
+    }
+});
 
 
 module.exports = router;
